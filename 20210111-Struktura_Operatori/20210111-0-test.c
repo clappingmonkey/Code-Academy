2365e3c42d7c291112bc6eba4b4f96bf0317aef8
clappingmonkey <clappingmonkey33@gmail.com>
2021-01-12 00:02:52 +0200

    done the task 0 different way

--- 20210111-Struktura_Operatori/20210111-0-test.c
+++ 20210111-Struktura_Operatori/20210111-0-test.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 /* the funciont for the addition  */
-int res(int a, int b, int c){
-    c = a + b;
-	return c;
-    }
+int res(int a, int b);
 
 int main(void){
     int a = 3;
     int b = 7;
-    int c = 1;
     /* declaring int var and directly attaching the value from res */
-    res(a,b,c);
+    res(a,b);
     /* printing the value of a and b and last their result from addition */
-    printf("The result of the addition between %d and %d is: %d\n", a, b, c);
+    printf("%d\n",res(a,b));
     return 0;
 }
+
+int res(int a, int b){
+    a+=b;
+	return a;
+    }
--- 20210111-Struktura_Operatori/20210111-0.c
+++ 20210111-Struktura_Operatori/20210111-0.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
 /* the funciont for the addition  */
-int res(int a, int b){
-	return a+b;
-}
+int AddDigit(int a, int b);
 
 int main(void){
     int a = 3;
     int b = 7;
-    /* declaring int var and directly attaching the value from res */
-    int c = res(a, b);
+    AddDigit(a, b);
     /* printing the value of a and b and last their result from addition */
-    printf("The result of the addition between %d and %d is: %d\n", a, b, c );
+    printf("The result of the addition between %d and %d is: %d\n", a, b, AddDigit(a,b));
     return 0;
+}
+
+/* the body of the addition function */
+int AddDigit(int a, int b){
+	a+=b;
+    return a;
 }
