83a80ff50e7c3d708aa7974c55dc56191a11f773
clappingmonkey <clappingmonkey33@gmail.com>
2021-01-16 13:13:15 +0200

    11th task done

--- 20210115-promenlivi_nizove/20210115-11.c
+++ 20210115-promenlivi_nizove/20210115-11.c
@@ -2,7 +2,14 @@
 
 int main(void){
     char str[] = "Hello";
-    char rts[] = "olleH";
+    printf("The original string is %s\n", str);
+    char rts[5];
+    rts[4] = str[0];
+    rts[3] = str[1];
+    rts[2] = str[2];
+    rts[1] = str[3];
+    rts[0] = str[4];
+    rts[5] = str[5];
+    printf("And after the change is %s\n", rts);
     return 0;
-
 }
