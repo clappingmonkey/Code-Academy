678c410884f2854b92bd93f389d6491a9b4c14f3
clappingmonkey <clappingmonkey33@gmail.com>
2021-01-16 14:35:58 +0200

    add 2nd and 3rd task done

--- 20210115-promenlivi_nizove/20210115-3.c
+++ 20210115-promenlivi_nizove/20210115-3.c
@@ -4,6 +4,6 @@ main() {
     printf("Character literals: '%c', '%c', '%c' \n", '\041', '\x3A', 'Z');
     printf("Integer literals: %d, %d, %ld \n", 123, 0xFFF, 123456789012345ULL);
     printf("Floating-point literals: %lf, %lf, %lg, %lf \n", 1.23, 1234.5678, 123E-12, 123E+28);
-    printf("String literals: \"%s\" , \"%s\"\n", "xxx", "yyy", "zzz");
+    printf("String literals: \"%s\" , \"%s\", \"%s\" \n", "xxx", "yyy", "zzz");
     return 0;
 }
