230a13851d5dc074cf76afa730962e5b760758a3
clappingmonkey <clappingmonkey33@gmail.com>
2021-01-16 13:56:46 +0200

    16th task done

--- 20210115-promenlivi_nizove/20210115-16.c
+++ 20210115-promenlivi_nizove/20210115-16.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
 const float c_pi = 3.14159;
-float liceOkr();
+float liceOkr(float r);
+
 int main(void) {
-    
+    float r = 1;
+    printf("The face of a circle with radius 1 is %f\n", liceOkr(r));
+    r = 1.5;
+    printf("The face of a circle with radius 1.5 %f\n", liceOkr(r));
+    r = 13;
+    printf("The face of a circle with radius 13 %f\n", liceOkr(r));
     return 0;
 
 }
 
-
-float liceOkr(){
+float liceOkr(float r){
     float lice;
-    lice = c_pi * r1 * r2;
+    lice = c_pi * r * r;
     return lice;
-
 }
