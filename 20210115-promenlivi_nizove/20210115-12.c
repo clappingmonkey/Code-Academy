fba908e03a01e2582b76629436f608fa391b34b6
clappingmonkey <clappingmonkey33@gmail.com>
2021-01-16 13:01:05 +0200

    12th task done

--- 20210115-promenlivi_nizove/20210115-12.c
+++ 20210115-promenlivi_nizove/20210115-12.c
@@ -2,7 +2,9 @@
 
 int main(void){
     char str[]= "Hello";
-    str[2,3] = "x";
-    printf("")
+    printf("The original str string - %s\n", str);
+    str[2] = 'x';
+    str[3] = 'x';
+    printf("Changed 2 and 3 position with x, the result - %s\n", str);
     return 0;
 }
