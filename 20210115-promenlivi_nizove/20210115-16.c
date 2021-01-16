ef6c11ff5a7f5e60eac489d4bc97eccb0571d09d
clappingmonkey <clappingmonkey33@gmail.com>
2021-01-16 14:09:24 +0200

    fix a typo in 16th task. add 17th task done

--- 20210115-promenlivi_nizove/20210115-16.c
+++ 20210115-promenlivi_nizove/20210115-16.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 const float c_pi = 3.14159;
-float liceOkr(float r);
+float liceOkr(float R);
 
 int main(void) {
-    float r = 1;
-    printf("The face of a circle with radius 1 is %f\n", liceOkr(r));
-    r = 1.5;
-    printf("The face of a circle with radius 1.5 %f\n", liceOkr(r));
-    r = 13;
-    printf("The face of a circle with radius 13 %f\n", liceOkr(r));
+    float R = 1;
+    printf("The face of a circle with radius %.0f is %f\n", R, liceOkr(R));
+    R = 1.5;
+    printf("The face of a circle with radius %.1f %f\n", R, liceOkr(R));
+    R = 13;
+    printf("The face of a circle with radius %0.f %f\n", R, liceOkr(R));
     return 0;
 
 }
 
-float liceOkr(float r){
+float liceOkr(float R){
     float lice;
-    lice = c_pi * r * r;
+    lice = c_pi * R * R;
     return lice;
 }
--- 20210115-promenlivi_nizove/20210115-17.c
+++ 20210115-promenlivi_nizove/20210115-17.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
 
 const float c_pi = 3.14159;
-float liceOkr();
+float liceOkr(float R);
+float liceElip(float A, float B);
+
 int main(void) {
-    
+    float R = 1;
+    float A = 2.3;
+    float B = 3.7;
+    printf("The face of a circle with radius %.0f is %f\n", R, liceOkr(R));
+    R = 1.5;
+    printf("The face of a circle with radius %.1f %f\n", R, liceOkr(R));
+    R = 13;
+    printf("The face of a circle with radius %.0f %f\n", R, liceOkr(R));
+    printf("The face of a Ellipse with the value of A %.1f and value of B %.1f is %f\n", A, B, liceElip(A,B));
     return 0;
 
 }
 
+float liceOkr(float R){
+    float liceOkrRez;
+    liceOkrRez = c_pi * R * R;
+    return liceOkrRez;
+}
 
-float liceOkr(){
-    float lice;
-    lice = c_pi * r1 * r2;
-    return lice;
-
+float liceElip(float A, float B){
+    float liceElRez;
+    liceElRez = c_pi * A * B;
+    return liceElRez;
 }
