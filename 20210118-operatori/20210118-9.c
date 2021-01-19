c973d14f1969899ba9fed5a11152569b9dce72c1
clappingmonkey <clappingmonkey33@gmail.com>
2021-01-19 11:52:24 +0200

    9th task done

--- 20210118-operatori/20210118-9.c
+++ 20210118-operatori/20210118-9.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
 
-int main(){
-    int nA;
-    printf("Enter value for nA:\n");
-    scanf("%d", &nA);
-    int nB = (nA == 1 ? 2 : 0); /* сравнява числото */
-    printf("A value is %d\n", nA); printf("B value is %d\n", nB);
-    }
+/* Задача 9. Аритметични оператори */
+
+int main(void)
+{
+    int iX = 13;
+    int iY = 44;
+    float fZ = 3.5;
+    int iResult = 0;
+    float fResult = 0.0;
+    
+    iResult = iX - iY;
+    printf("%d - %d = %d\n", iX, iY, iResult);
+    iResult = iY / iX;
+    printf("%d / %d = %d\n", iY, iX, iResult);
+    iResult = iY % iX;
+    printf("%d / %d Remainder: %d\n", iY, iX, iResult);
+
+    iResult = iX + iY;
+    printf("%d + %d = %d\n", iX, iY, iResult);
+    iResult = iX * iY;
+    printf("%d * %d = %d\n", iX, iY, iResult);
+
+    fResult = fZ + iX + iY;
+    printf("%.1f + %d + %d = %.2f\n", fZ, iX, iY, fResult);
+    fResult = fZ * iX * iY;
+    printf("%.1f * %d * %d = %.2f\n", fZ, iX, iY, fResult);
+
+    fResult = iX + iY + iResult;
+    printf("%d + %d + %d = %.2f\n", iX, iY, iResult, fResult);
+    fResult = iX * iY * iResult;
+    printf("%d * %d * %d = %.2f\n", iX, iY, iResult, fResult);
+
+    return 0;
+}
