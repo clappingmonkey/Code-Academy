d5cbaf78b54d87d4edac97bc125ff8f6763d7b5b
clappingmonkey <clappingmonkey33@gmail.com>
2021-01-19 13:36:06 +0200

    fixed 5th task

--- 20210118-operatori/20210118-5.c
+++ 20210118-operatori/20210118-5.c
@@ -4,10 +4,11 @@ int main()
 {
     int x = 1, y = 0;
     int z = y || x;
+    printf("The value of Z is %d\n", z);
 
-    printf("\nInit values: X = %d, Y = %d\n", x, y);
+    printf("Init values: X = %d, Y = %d\n", x, y);
     x = 1 + 2, 2 * 3, 3 + 4;
     y = (7 * 8, 8 + 9, 9 - 4);
-    printf("\nX = %d, Y = %d\n", x, y);
+    printf("X = %d, Y = %d\n", x, y);
     return 0;
 }
