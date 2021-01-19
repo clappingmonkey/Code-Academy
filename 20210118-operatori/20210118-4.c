592d1ea37c5d89605535403fa8b291efe1e040ad
clappingmonkey <clappingmonkey33@gmail.com>
2021-01-19 14:10:29 +0200

    add 4th task done

--- 20210118-operatori/20210118-4.c
+++ 20210118-operatori/20210118-4.c
@@ -6,11 +6,21 @@ int main()
     int y = 10;
     int true, false;
 
-    true = ++x + x || y;
+    true = ++x || y;
     printf("The value of true is %d\n", true);
     true = x && y & x << 1;
     printf("The value of true is %d\n", true);
     true = x && y ^ x << 1;
+    printf("The value of true is %d\n", true);
+    /* първо ще се извърши шифтването на ляво,
+    след това побитовото И и накрая логическото И.
+    Ще се принтира 1 като резултат */
+    true = x&&y&x<<1;
+    printf("The value of true is %d\n", true);
+    /* първо ще се изръвши шифтването на ляво,
+    след това XOR и накрая логическото И.
+    Резултата ще бъде 1 */
+    true = x&&y^x<<1;
     printf("The value of true is %d\n", true);
     return 0;
 }
